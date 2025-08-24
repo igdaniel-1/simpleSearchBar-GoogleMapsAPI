884da6f37b3dc7740f31ac8bd8e6aa59e9766551
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-08-24 18:05:41 -0400

    searches for restaurants in san fran

---
+++ index.ts
@@ -0,0 +1,47 @@
+async function init() {
+    // @ts-ignore
+    const { Place, AutocompleteSessionToken, AutocompleteSuggestion } = await google.maps.importLibrary("places") as google.maps.PlacesLibrary;
+
+    // Add an initial request body.
+    let request = {
+        input: "Hello",
+        locationRestriction: { west: -122.44, north: 37.8, east: -122.39, south: 37.78 },
+        origin: { lat: 37.7893, lng: -122.4039 },
+        includedPrimaryTypes: ["restaurant"],
+        language: "en-US",
+        region: "us",
+    };
+
+    // Create a session token.
+    const token = new AutocompleteSessionToken();
+    // Add the token to the request.
+    // @ts-ignore
+    request.sessionToken = token;
+    // Fetch autocomplete suggestions.
+    const { suggestions } = await AutocompleteSuggestion.fetchAutocompleteSuggestions(request);
+
+    const title = document.getElementById('title') as HTMLElement;
+    title.appendChild(document.createTextNode('Query predictions for "' + request.input + '":'));
+
+    for (let suggestion of suggestions) {
+        const placePrediction = suggestion.placePrediction;
+
+        // Create a new list element.
+        const listItem = document.createElement('li');
+        const resultsElement = document.getElementById("results") as HTMLElement;
+        listItem.appendChild(document.createTextNode(placePrediction.text.toString()));
+        resultsElement.appendChild(listItem);
+    }
+
+    let place = suggestions[0].placePrediction.toPlace(); // Get first predicted place.
+    await place.fetchFields({
+        fields: ['displayName', 'formattedAddress'],
+    });
+
+    const placeInfo = document.getElementById("prediction") as HTMLElement;
+    placeInfo.textContent = 'First predicted place: ' + place.displayName + ': ' + place.formattedAddress;
+
+}
+
+init();
+export { };
